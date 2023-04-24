7a08d614b54bee2765d7817575109aa2582457bb
Ben Craven <ben@cravenzone.ca>
2023-04-24 11:25:11 -0400

    v3.7.1

--- ProtoFiles/Exec/postversion.ts
+++ ProtoFiles/Exec/postversion.ts
@@ -2,15 +2,15 @@ import promExec from "./promExec"
 import { version, latestLibraryVersion } from "../../package.json"
 
 async function main() {
-	await promExec(`git tag v${version}`)
+	console.log(await promExec(`git tag v${version}`))
 
-	await promExec("git push")
+	console.log(await promExec("git push"))
 
-	await promExec("git push --tags")
+	console.log(await promExec("git push --tags"))
 
-	await promExec("clasp push")
+	console.log(await promExec("clasp push"))
 
-	await promExec(`clasp deploy -V ${latestLibraryVersion + 1} -d "v${version}"`)
+	console.log(await promExec(`clasp deploy -d "v${version}"`))
 }
 
 main()
--- ProtoFiles/Exec/version.ts
+++ ProtoFiles/Exec/version.ts
@@ -3,17 +3,17 @@ import promExec from "./promExec"
 import { resolve as R } from "path"
 
 async function main() {
-	await promExec(`ts-node ${R(__dirname, "./getLatestLibraryVersion.ts")}`)
+	console.log(await promExec(`ts-node ${R(__dirname, "./getLatestLibraryVersion.ts")}`))
 
-	await promExec(`ts-node ${R(__dirname, "./updateMain.ts next")}`)
+	console.log(await promExec(`ts-node ${R(__dirname, "./updateMain.ts next")}`))
 
-	await promExec(`call ${R(__dirname, "./typeify.bat")}`)
+	console.log(await promExec(`call ${R(__dirname, "./typeify.bat")}`))
 
-	await promExec(`call ${R(__dirname, "./transpile.bat")}`)
+	console.log(await promExec(`call ${R(__dirname, "./transpile.bat")}`))
 
-	await promExec(`git add .`)
+	console.log(await promExec(`git add .`))
 
-	await promExec(`git commit -m "v${version}"`)
+	console.log(await promExec(`git commit -m "v${version}"`))
 }
 
 main()
